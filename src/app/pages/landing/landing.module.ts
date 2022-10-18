916574a87df5ccbe1f8e26ddf45c1695a6918136
AliZoghi <alizoghiwork@gmail.com>
2022-10-19 01:12:30 +0330

    [ADD] basic of hero section

--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LandingComponent} from './landing.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 
 @NgModule({
-    declarations: [LandingComponent],
+    declarations: [LandingComponent, HeroSectionComponent],
     imports: [CommonModule],
 })
 export class LandingModule {}
