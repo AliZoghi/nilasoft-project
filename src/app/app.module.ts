42deedccc11ed5074bed46069bc485671530af05
AliZoghi <alizoghiwork@gmail.com>
2022-10-18 18:38:23 +0330

    [ADD] first layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {LandingModule} from './pages/landing/landing.module';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, NavbarComponent, FooterComponent],
+    imports: [BrowserModule, AppRoutingModule, LandingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
