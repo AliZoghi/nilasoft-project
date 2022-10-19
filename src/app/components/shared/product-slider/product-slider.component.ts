8cc9c02e73a2d74ffd0b20c09a2fd59fab1b2e1b
AliZoghi <alizoghiwork@gmail.com>
2022-10-20 01:42:32 +0330

    [ADD] draggable product-slider

--- src/app/components/shared/product-slider/product-slider.component.ts
+++ src/app/components/shared/product-slider/product-slider.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {ProductItem, ProductItemFake} from '../../../models/ProductItem.interface';
 
 @Component({
@@ -6,10 +6,52 @@ import {ProductItem, ProductItemFake} from '../../../models/ProductItem.interfac
     templateUrl: './product-slider.component.html',
     styleUrls: ['./product-slider.component.scss'],
 })
-export class ProductSliderComponent {
+export class ProductSliderComponent implements AfterViewInit {
     public productItems: ProductItem[] = ProductItemFake;
-
     @ViewChild('slider') public sliderContainer!: ElementRef;
+
+    public ngAfterViewInit(): void {
+        let isDown = false;
+        let startX: number;
+        let scrollLeft: number;
+        let gutter: number;
+        const slider = this.sliderContainer.nativeElement;
+        console.log(slider);
+        const end = (): void => {
+            isDown = false;
+            const extraStep = slider.scrollLeft % gutter;
+            if (extraStep > gutter / 2) {
+                slider.scrollLeft = slider.scrollLeft - extraStep + gutter;
+            } else {
+                console.log('salam2');
+                slider.scrollLeft = slider.scrollLeft - extraStep;
+            }
+        };
+
+        const start = (e: any): void => {
+            gutter = this.calculateSpaceOfBetweenCards();
+            isDown = true;
+            console.log(isDown);
+            startX = e.pageX;
+            console.log(startX);
+            scrollLeft = slider.scrollLeft;
+            console.log(scrollLeft);
+            console.log('------');
+        };
+
+        const move = (e: any): void => {
+            if (!isDown) return;
+
+            e.preventDefault();
+            const x = e.pageX;
+            const dist = x - startX;
+            slider.scrollLeft = scrollLeft - dist;
+        };
+
+        slider.addEventListener('mousedown', start);
+        slider.addEventListener('mousemove', move);
+        slider.addEventListener('mouseup', end);
+    }
 
     public scrollFunction(goRight: -1 | 1): void {
         const space = this.calculateSpaceOfBetweenCards() * goRight;
--- src/app/models/ProductItem.interface.ts
+++ src/app/models/ProductItem.interface.ts
@@ -36,7 +36,29 @@ export const ProductItemFake: ProductItem[] = [
         },
     },
     {
-        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-python.jpg',
+        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-angular.jpg',
+        title: 'The Python Course: build web application',
+        time: 150,
+        rate: {averageRate: 4.5, count: 13245},
+        level: ProductLevelEnum.intermediate,
+        teacher: {
+            name: 'Claire Robertson',
+            image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
+        },
+    },
+    {
+        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-graphql.jpg',
+        title: 'The Python Course: build web application',
+        time: 150,
+        rate: {averageRate: 4.5, count: 13245},
+        level: ProductLevelEnum.intermediate,
+        teacher: {
+            name: 'Claire Robertson',
+            image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
+        },
+    },
+    {
+        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-react.jpg',
         title: 'The Python Course: build web application',
         time: 150,
         rate: {averageRate: 4.5, count: 13245},
@@ -58,29 +80,7 @@ export const ProductItemFake: ProductItem[] = [
         },
     },
     {
-        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-python.jpg',
-        title: 'The Python Course: build web application',
-        time: 150,
-        rate: {averageRate: 4.5, count: 13245},
-        level: ProductLevelEnum.intermediate,
-        teacher: {
-            name: 'Claire Robertson',
-            image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
-        },
-    },
-    {
-        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-python.jpg',
-        title: 'The Python Course: build web application',
-        time: 150,
-        rate: {averageRate: 4.5, count: 13245},
-        level: ProductLevelEnum.intermediate,
-        teacher: {
-            name: 'Claire Robertson',
-            image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
-        },
-    },
-    {
-        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-python.jpg',
+        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-graphql.jpg',
         title: 'The Python Course: build web application',
         time: 150,
         rate: {averageRate: 4.5, count: 13245},
