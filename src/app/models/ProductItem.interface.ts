aac77ef9c05e2a947fe4b47e3a269ed11e4a16f4
AliZoghi <alizoghiwork@gmail.com>
2022-10-19 20:39:01 +0330

    [ADD] product-slider appearance

--- src/app/models/ProductItem.interface.ts
+++ src/app/models/ProductItem.interface.ts
@@ -57,4 +57,37 @@ export const ProductItemFake: ProductItem[] = [
             image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
         },
     },
+    {
+        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-python.jpg',
+        title: 'The Python Course: build web application',
+        time: 150,
+        rate: {averageRate: 4.5, count: 13245},
+        level: ProductLevelEnum.intermediate,
+        teacher: {
+            name: 'Claire Robertson',
+            image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
+        },
+    },
+    {
+        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-python.jpg',
+        title: 'The Python Course: build web application',
+        time: 150,
+        rate: {averageRate: 4.5, count: 13245},
+        level: ProductLevelEnum.intermediate,
+        teacher: {
+            name: 'Claire Robertson',
+            image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
+        },
+    },
+    {
+        image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/course/course-python.jpg',
+        title: 'The Python Course: build web application',
+        time: 150,
+        rate: {averageRate: 4.5, count: 13245},
+        level: ProductLevelEnum.intermediate,
+        teacher: {
+            name: 'Claire Robertson',
+            image: 'https://codescandy.com/geeks-bootstrap-5/assets/images/avatar/avatar-4.jpg',
+        },
+    },
 ];
