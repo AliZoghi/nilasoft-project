75044669449de54736e7baa5cee3c2da6d218d76
AliZoghi <alizoghiwork@gmail.com>
2022-10-19 17:51:47 +0330

    [ADD] information-component

--- src/app/pages/landing/components/information/information.component.ts
+++ src/app/pages/landing/components/information/information.component.ts
@@ -9,5 +9,7 @@ import {InformationCard} from '../../../../models/InformationCard.interface';
 export class InformationComponent {
     public dataCards: InformationCard[] = [
         {iconType: 'video', title: '30,000 online courses', description: 'Enjoy a variety of fresh topics'},
+        {iconType: 'users', title: 'Expert instruction', description: 'Find the right instructor for you'},
+        {iconType: 'clock', title: 'Lifetime access', description: 'Learn on your schedule'},
     ];
 }
