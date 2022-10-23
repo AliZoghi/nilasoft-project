f35381bc781b8df3e2f6b8094c1d8779902c9deb
AliZoghi <alizoghiwork@gmail.com>
2022-10-23 10:00:24 +0330

    [FIX] detect mouse move,click event on slider

--- src/app/components/shared/product-card/product-card.component.ts
+++ src/app/components/shared/product-card/product-card.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {ProductItem} from '../../../models/ProductItem.interface';
-
+import {Router} from '@angular/router';
+interface MouseEventInterface {
+    x: number;
+    y: number;
+}
 @Component({
     selector: 'app-product-card',
     templateUrl: './product-card.component.html',
@@ -8,4 +12,22 @@ import {ProductItem} from '../../../models/ProductItem.interface';
 })
 export class ProductCardComponent {
     @Input() public productItem!: ProductItem;
+    private mousedownLocation!: MouseEventInterface;
+
+    public constructor(private router: Router) {}
+
+    public storeMousedownLocation(e: MouseEvent): void {
+        const {clientX: x, clientY: y} = e;
+        this.mousedownLocation = {x, y};
+    }
+
+    public checkValidRouteAndNavigate(e: MouseEvent, id: number): void {
+        const {clientX: x, clientY: y} = e;
+        const {x: downX, y: downY} = this.mousedownLocation;
+        if (x === downX && y === downY) this.navigateProduct(id);
+    }
+
+    private async navigateProduct(id: number): Promise<void> {
+        await this.router.navigate(['/product', id]);
+    }
 }
